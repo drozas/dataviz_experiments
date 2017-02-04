93a544eec2a4e8fd75bf716943757e190ddee4f8
David Rozas <david.rozas@gmail.com>
2017-02-04 13:21:40 +0100

    Swapping code

--- tweets_barchar.js
+++ tweets_barchar.js
@@ -2,62 +2,39 @@ d3.json("tweets.json", function(error,data) {dataViz(data.tweets)});
 
 function dataViz(incomingData) {
 	
+	//Gather tweets under author
+	var nestedTweets = d3.nest()
+		.key(function (el) {return el.user;})
+		.entries(incomingData);				
+	
 	//Create a new attribute based on the number of tweets
-	incomingData.forEach(function (el) {
-		el.impact = el.favorites.length + el.retweets.length;
-		//Transform the ISO date into a data type
-		el.tweetTime = new Date(el.timestamp);
+	nestedTweets.forEach(function (el) {
+		el.numTweets = el.values.length;
 	})
 	
-	//Define max function according to impact
-	var maxImpact = d3.max(incomingData, function (el) {
-		return el.impact;}
-	);
-
-	//Return earliest and latest tweets in an array
-	var startEnd = d3.extent(incomingData, function (el) {
-		return el.tweetTime;}
+	//Define max function
+	var MaxTweets = d3.max(nestedTweets, function (el) {
+		return el.numTweets;}
 	);
 	
-	//Define time scale / x (for v4)
-	var timeRamp = d3.scaleTime().domain(startEnd).range([20,480]);
+	//Define scale (for v4)
+	var yScale = d3.scaleLinear().domain([0, MaxTweets]).range([0,100]);
 	
-	//Define y scale (for v4)
-	var yScale = d3.scaleLinear().domain([0, maxImpact]).range([0,460]);
-	
-	//Define radius scale (for v4)
-	var radiusScale = d3.scaleLinear().domain([0, maxImpact]).range([1,20]);
-	
-	//Define color scale (for v4) - from white to red
-	var colorScale = d3.scaleLinear().domain([0, maxImpact]).range(["white","#990000"]);
-	
-	//d3.select("svg")
-	//	.attr("style", "height: 480px; width: 600px;");
-	
-	// Tweets are represented as cirles sized by the total number of favs and rts, and 
-	// placed on the canvas along the x axis based on the time of the tweet and along the 
-	// y axis according to the same impact factor used for the size of the circles
 	d3.select("svg")
-		.selectAll("circle")
-		.data(incomingData)
+		.attr("style", "height: 480px; width: 600px;");
+		
+	d3.select("svg")
+		.selectAll("rect")
+		.data(nestedTweets)
 		.enter()
-		.append("circle")
-		.attr("r", function (d) {return radiusScale(d.impact);})
-		.attr("cx", function (d,i) {return timeRamp(d.tweetTime);})
-		.attr("cy", function (d) {return 480 - yScale(d.impact);})
-		.style("fill", function (d) {return colorScale(d.impact);})
-		.style("stroke", "black")
-		.style("stroke-width", "1px");
-	
-	var tweetG = d3.select("svg")
-		.selectAll("g")
-		.data(incomingData)
-		.enter()
-		.append("g")
-		.attr("transform", function(d) {
-			return "translate(" + 
-			timeRamp(d.tweetTime) + "," + (480 - yScale(d.impact))
-			+ ")"
-		});
+		.append("rect")
+		.attr("width", 50)
+		.attr("height", function (d) {return yScale(d.numTweets);})
+		.attr("x", function (d,i) {return i * 60;})
+		.attr("y", function (d) {return 480 - yScale(d.numTweets);})
+		.style("fill", "blue")
+		.style("stroke", "red")
+		.style("stroke-width", "1px")
+		.style("opacity", .25);
 
 }
